0c6e9c8dffc513f6cdbb4b8d4984f94e8f916af0
NicolasSilvaMat17 <nicolassilvamat@gmail.com>
2026-07-31 21:10:11 -0300

    Estrutura de controle de seleção, estrutura for e exercício 03 - Parque de diversões

---
+++ Lógica de Programação e Algoritimos/Aula06/Lanchonete/ParOuImpar/Program.cs
@@ -0,0 +1,33 @@
+﻿// Declaração de variáveis
+Console.Write("Quantos números você vai digitar: ");
+int n = int.Parse(Console.ReadLine());
+
+// Controle de repetição
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Digite um número: ");
+    int numero = int.Parse(Console.ReadLine());
+
+    if (numero < 0)
+    {
+        Console.WriteLine("Negativo");
+    }
+    else if (numero == 0)
+    {
+        Console.WriteLine("Nulo");
+        continue;
+    }
+    {
+        Console.WriteLine("Positivo");
+    }
+
+    if (numero % 2 > 0)
+    {
+        Console.WriteLine("Par");
+
+    } 
+    else
+    {
+        Console.WriteLine("Impar");
+    }
+}
---
+++ Lógica de Programação e Algoritimos/Aula06/Lanchonete/ParqueDeDivercoes/Program.cs
@@ -0,0 +1,39 @@
+﻿// Declaração de variáveis
+int contagem = 0;
+Console.Write("Digite a quantidade de pessoas: ");
+int visitantes = int.Parse(Console.ReadLine());
+
+Console.Write("Digite a altura minima permitida (em cm): ");
+double altura_minima = double.Parse(Console.ReadLine());
+Console.Write("Digite a altura maxima permitida (em cm): ");
+double altura_maxima = double.Parse(Console.ReadLine());
+
+try
+{
+    for (int i = 0; i < visitantes; i++)
+    {
+        Console.Write("Digite a altura dos visitantes: ");
+        double alturaVisitantes = double.Parse(Console.ReadLine());
+        if (alturaVisitantes < altura_minima) 
+        {
+            Console.WriteLine("Altura minima insuficiente");
+        }
+        else if (alturaVisitantes > altura_maxima)
+        {
+            Console.WriteLine("A altura está acima do permitido ");
+        }
+        else
+        {
+            Console.WriteLine("Acesso permitido");
+            contagem++;
+        }
+        
+    }
+
+    Console.Write($"Total de pessoas que podem andar na montanha-russa: {contagem}");
+}
+catch
+{
+    Console.WriteLine("Valor invaldo, digite um número inteiro.");
+}
+
