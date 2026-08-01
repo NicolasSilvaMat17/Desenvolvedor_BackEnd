bdca2645c23220e25a810ae7b3a368a9eb0ef370
NicolasSilvaMat17 <nicolassilvamat@gmail.com>
2026-07-31 21:54:25 -0300

    Aula de sexta feira, feito for, while, do while, switch case

---
+++ Lógica de Programação e Algoritimos/Aula06/Lanchonete/Senha/Program.cs
@@ -0,0 +1,16 @@
+﻿// Declaração de variaveis
+
+const int senha = 2002;
+
+// Entrada de ddados
+Console.WriteLine("Digite a senha: ");
+int n = int.Parse(Console.ReadLine());
+
+while (senha != n)
+{
+    Console.Write("Senha Invalida, tente novamente: ");
+    n = int.Parse(Console.ReadLine());
+    int++;
+    Console.WriteLine("Número de tentativas");
+}
+Console.WriteLine("Acesso permitido!");
---
+++ Lógica de Programação e Algoritimos/Aula06/Lanchonete/Sequencia/Program.cs
@@ -0,0 +1,21 @@
+﻿// Declação de variáveis
+int x;
+
+// Entrada de dados 
+Console.WriteLine("Digite o valor de X: ");
+x = int.Parse(Console.ReadLine());
+
+
+
+for (int i = 1; i <= x; i++)
+{
+    if (i % 2 != 0)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+foreach(int item in x)
+{
+
+}
