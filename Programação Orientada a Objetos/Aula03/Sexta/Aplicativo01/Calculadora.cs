6a649cf782fb4e7ca8928e19319aa57a724d83af
NicolasSilvaMat17 <nicolassilvamat@gmail.com>
2026-08-14 19:48:53 -0300

    Versão 2

---
+++ Programação Orientada a Objetos/Aula03/Sexta/Aplicativo01/Calculadora.cs
@@ -0,0 +1,30 @@
+﻿namespace Aplicativo01
+{
+    internal class Calculadora
+    {
+        // Membros 
+        // 1° - Campos
+        double raio;
+        public const double pi = 3.1415;
+
+        // 2° - Construtor
+        public Calculadora(double raio)
+        {
+            this.raio = raio;
+        }
+        // 3° - Métodos
+        public double Circunferencia()
+        {
+            return 2 * pi * raio;
+        }
+        public double Volume()
+        {
+            return (4 * pi * Math.Pow(raio, 3)) / 3;
+        }
+
+        public void PI()
+        {
+            Console.WriteLine(pi);
+        }
+    }
+}
