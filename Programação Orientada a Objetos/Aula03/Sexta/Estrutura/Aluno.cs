a7881010a6464af8ad428b37983fa30c7e6a9f36
NicolasSilvaMat17 <nicolassilvamat@gmail.com>
2026-08-14 19:26:19 -0300

    Versão 1

---
+++ Programação Orientada a Objetos/Aula03/Sexta/Estrutura/Aluno.cs
@@ -0,0 +1,30 @@
+﻿namespace Estrutura
+{
+    internal struct Aluno
+    {
+        // Membros de uma struct
+        //  Campos
+        public string nome;
+        public int cpf;
+        public double altura;
+
+        // Construtor
+        public Aluno(string nome, int cpf) : this()
+        {
+            this.nome = nome;
+            this.cpf = cpf;
+        }
+
+        public Aluno(string nome, int cpf, double altura)
+        {
+            this.nome = nome;
+            this.cpf = cpf;
+            this.altura = altura;
+        }
+        // Métodos
+        public double Idade(int idade)
+        {
+            return idade / 365;
+        }
+    }
+}
