fb033bdb17353b3fdbd260392c150c442153e4a4
NicolasSilvaMat17 <nicolassilvamat@gmail.com>
2026-08-14 21:18:50 -0300

    Exercício classe estática

---
+++ Programação Orientada a Objetos/Aula03/Sexta/Moeda/Dolares.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Moeda
+{
+    internal static class Dolares
+    {
+        // Membros de uma classe
+        // 1° Campo
+        public static double dolares;
+
+        // 3° metodos
+        public const double IOF = 0.06;
+
+        public static double Comprar(double cotacao, double compra)
+        {
+            double pagamento = (cotacao * compra * (1 + IOF));
+            return pagamento;
+        }
+    }
+
+}
---
+++ Programação Orientada a Objetos/Aula03/Sexta/Moeda/Program.cs
@@ -0,0 +1,14 @@
+﻿// Declaração de variáveis
+
+using Moeda;
+
+double dolar, cotacao, reais;
+
+// Entrada de dados 
+Console.WriteLine("Digit a cotação do dólar: ");
+cotacao = double.Parse(Console.ReadLine());
+Console.WriteLine("Quantos sólares você vai comprar? ");
+dolar = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Valor a ser pago em reais = {Dolares.Comprar(cotacao, dolar):c}");
+
