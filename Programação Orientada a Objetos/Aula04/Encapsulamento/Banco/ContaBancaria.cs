bfeb1b60d9133e601763f78883415a40a8f18b56
NicolasSilvaMat17 <nicolassilvamat@gmail.com>
2026-08-17 21:51:27 -0300

    Feito encapsulamento e tela de cadastro em banco

---
+++ Programação Orientada a Objetos/Aula04/Encapsulamento/Banco/ContaBancaria.cs
@@ -0,0 +1,83 @@
+﻿using Banco;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Banco
+{
+    public class ContaBancaria
+    {
+        // 1° - Campos
+        private string titular;
+        private int numero;
+        private double saldo;
+
+
+        // 2° - Propriedade 
+        protected string TitularDaConta
+        {
+            get { return titular; }
+            set
+            {
+                if (value != null | value != "")
+                {
+                    titular = value;
+                }
+                else
+                {
+                    Console.WriteLine("Valor de campo inválido");
+                }
+            }
+
+        }
+
+        protected int NumeroDaConta
+        {
+            get { return numero; }
+            private set { numero = value; }
+        }
+
+        protected double SaldoDaConta
+        {
+            get { return saldo; }
+            set { saldo = value; }
+        }
+        // 3° - Construtor
+        public ContaBancaria(string titularDaConta, int numeroDaConta, double saldoDaConta)
+        {
+            TitularDaConta = titularDaConta;
+            NumeroDaConta = numeroDaConta;
+            SaldoDaConta = saldoDaConta;
+        }
+
+        public ContaBancaria(string titularDaConta, int numeroDaConta)
+        {
+            TitularDaConta = titularDaConta;
+            NumeroDaConta = numeroDaConta;
+            SaldoDaConta = 0.0;
+        }
+
+        // 4° - Métodos
+        public void Deposito(double quantia)
+        {
+            SaldoDaConta += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            if (quantia <= 0)
+            {
+                Console.WriteLine("Valor invalido, insira valores positivos ou diferentes de zero: ");
+            }
+            else
+            {
+                SaldoDaConta -= quantia + 5.00;
+            }
+        }
+        public string ExibirDados()
+        {
+            return $"Dados da conta: \n" +
+                $"\tTitular da Conta: {TitularDaConta}\n" + 
+                $"\tNúmero da conta: {NumeroDaConta}\n" +
+                $"\tSaldo da conta: {SaldoDaConta:c}\n";
+        }
+    }
+}
---
+++ Programação Orientada a Objetos/Aula04/Encapsulamento/Banco/Program.cs
@@ -0,0 +1,42 @@
+﻿using Banco;
+
+// Declaração de Variáveis
+string nome;
+int numero;
+double saldo, quantia;
+char escolha;
+ContaBancaria conta1;
+
+// Entrada de dados
+Console.Write("Entre com o número da conta: ");
+numero = int.Parse(Console.ReadLine());
+Console.Write("Entre com o titular da conta: ");
+nome = Console.ReadLine();
+Console.Write("Deseja fazer depósito incial (s/n) ? : ");
+escolha = Char.Parse(Console.ReadLine().ToLower());
+
+if (escolha == 's')
+{
+    Console.Write("Entre com o valor inicial: ");
+    saldo = double.Parse(Console.ReadLine());
+    conta1 = new ContaBancaria(nome, numero, saldo);
+}
+else
+{
+    conta1 = new ContaBancaria(nome, numero);
+}
+Console.WriteLine(conta1.ExibirDados());
+
+Console.Write("Entre com um valor para depósito: ");
+quantia  = double.Parse(Console.ReadLine());
+conta1.Deposito(quantia);
+Console.WriteLine("--Dados atualizados--");
+Console.WriteLine(conta1.ExibirDados());
+Console.Write("Entre com um valor para saque: ");
+quantia = double.Parse(Console.ReadLine());
+conta1.Saque(quantia);
+Console.WriteLine("--Dados atualizados--");
+Console.WriteLine(conta1.ExibirDados());
+
+
+Console.ReadKey();
